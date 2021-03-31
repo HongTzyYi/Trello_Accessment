9f2ba60dc6a92d74d60dc815bc92b2a0bb8aa31f
Tzy Yi Hong <TzyYi.Hong@grassvalley.com>
2021-03-31 13:24:45 +0800

    Add String period console application

---
+++ StringPeriods/StringPeriods/Program.cs
@@ -0,0 +1,71 @@
+﻿/*String Period Problem
+ *Have the function StringPeriods(str) take the str parameter being passed and determine if there is some substring K that can be repeated 
+ *N > 1 times to produce the input string exactly as it appears. Your program should return the longest substring K, and if there is none it 
+ *should return the string -1.
+ *For example: if str is "abcababcababcab" then your program should return abcab because that is the longest substring that is 
+ *repeated 3 times to create the final string. Another example: if str is "abababababab" then your program should return ababab because 
+ *it is the longest substring. If the input string contains only a single character, your program should return the string -1. 
+ */
+
+/*Logic Crafting:
+ *Assumption: 
+ *-For string to repeat itself, the max length for that string will always be 1/2 of given string length
+ *
+ *Using Recursion approach:
+ *-the starting string to check for would be substring of 1/2 length from given input
+ *-check whether the string is being repeated in the given input
+ *-If the string repeated more than once, end checking and return result
+ *-else call the function again, this time with the checking sting length -1 from previous checking
+ */
+
+using System;
+using System.Linq;
+
+namespace StringPeriods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            string testInput = "abababababab";
+            Console.WriteLine("Base string: " + testInput);
+            Console.WriteLine(program.StringPeriod(testInput));
+        }
+        /// <summary>
+        /// Base function to handle input checking and find longest repeating pattern in input
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string StringPeriod(string input)
+        {
+            if(input.Length <= 1)
+            {
+                return "-1";
+            }
+            return StringPeriod(input, input.Substring(0, input.Length / 2));
+        }
+        /// <summary>
+        /// Overloaded function to check for longest repeating pattern recursively
+        /// </summary>
+        /// <param name="BaseInput"></param>
+        /// <param name="TestInput"></param>
+        /// <returns></returns>
+        public string StringPeriod(string BaseInput, string TestInput = "")
+        {
+            //Count occurence of repeated pattern here
+            string[] ToCheck = BaseInput.Split(TestInput);
+            if(ToCheck.Length > 2 && ToCheck.All(i => i == ""))
+            {
+                return "Longest repeating pattern: " + TestInput;
+            }
+            
+            if(TestInput.Length <= 1)
+            {
+                return "No repeating pattern in this string!";
+            }
+
+            return StringPeriod(BaseInput, TestInput.Substring(0, TestInput.Length-1));
+        }
+    }
+}
