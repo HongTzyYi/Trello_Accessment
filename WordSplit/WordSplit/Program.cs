61beb278df07844fb28719658000871295e0f0e1
Tzy Yi Hong <TzyYi.Hong@grassvalley.com>
2021-03-31 10:19:24 +0800

    Add Recursive WordSplit Logic

--- WordSplit/WordSplit/Program.cs
+++ WordSplit/WordSplit/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace WordSplit
 {
@@ -10,7 +9,7 @@ namespace WordSplit
         static void Main(string[] args)
         {
             Program program = new Program();
-            string[] testArray = { "cat", "apple,bat,cat,goodbye,hell,heLlo,yellow,why,,zee,zzz" };
+            string[] testArray = { "hellozzz", "apple,bat,cat,goodbye,hell,heLlo,yellow,why,zee,zzz" };
 
             Array.ForEach<string>(testArray, Console.WriteLine);
             Console.WriteLine(program.WordSplit(testArray));
@@ -20,57 +19,54 @@ namespace WordSplit
         /// Split the first element in strArr into 2 words containing in the subsequent elements
         /// Assumes the following:
         /// -All inputs are in English
-        /// -Splitted words are case insensitive, example "HeLlO" will also be able to split from "helloworld"
-        /// -First array element is the word to split and second array element is the list of words to refer when splitting
+        /// -For parameters, first array element is the word to split and second array element is the list of words to refer when splitting
         /// </summary>
         /// <param name="strArr"></param>
         /// <returns></returns>
         public string WordSplit(string[] strArr)
         {
-            string result = string.Empty;
-
             string WordToSplit = strArr[0];
-
-
-            //Method 1: Double for loop, check by adding two matching words
-            //string[] ToCheck = strArr[1].Split(',');
-            //for (int i = 0; i < ToCheck.Length; i++)
+            string[] ToCheck = strArr[1].ToLower().Split(",");
+            //List<string> ToCheck2 = new List<string>();
+            //for(int i=0; i < ToCheck.Length; i++)
             //{
-            //    if (!string.IsNullOrEmpty(ToCheck[i]))
+            //    if (!string.IsNullOrEmpty(ToCheck[i]) && WordToSplit.ToLower().Contains(ToCheck[i]))
             //    {
-            //        if (WordToSplit.ToLower().Contains(ToCheck[i].ToLower()))
-            //        {
-            //            //Check if another word also contain within the remaining letters
-            //            for (int o = 0; o < ToCheck.Length; o++)
-            //            {
-            //                if (!string.IsNullOrEmpty(ToCheck[o]) && (ToCheck[i] + ToCheck[o]).ToLower() == WordToSplit.ToLower())
-            //                {
-            //                    result += ToCheck[i] + "," + ToCheck[o];
-            //                    return result; //Located the 2 words within WordToSplit. End the checking loop and return result
-            //                }
-            //            }
-            //        }
+            //        ToCheck2.Add(ToCheck[i]);
+            //        ToCheck2.Add(WordToSplit.Replace(ToCheck[i], ""));
             //    }
             //}
-            //return "Unable to split into 2 words from given list";
+            //var matchedvalue = ToCheck.Intersect(ToCheck2);
+            //return string.Join(",", matchedvalue);
 
-            //Method 2: one for loop only, done by obtaining the list of remaining letters from first match, then intersect the list with the provided list of words
-            List<string> ToCheck = new List<string>(strArr[1].ToLower().Split(","));
-            List<string> ToCheck2 = new List<string>();
-            for(int i=0; i < ToCheck.Count; i++)
+            //Split using recursive method
+            return IsWord(WordToSplit, ToCheck);
+        }
+        /// <summary>
+        /// Recursive function to identify one word from BaseWord using ReferenceWords list, split and return it into two words
+        /// </summary>
+        /// <param name="BaseWord"></param>
+        /// <param name="ReferenceWords"></param>
+        /// <returns></returns>
+        public string IsWord(string BaseWord, string[] ReferenceWords, int SplitFromLetterCount = 0)
+        {
+            string result = "Unable to split \"" + BaseWord + "\" from provided reference words list!";
+            
+            while(SplitFromLetterCount < BaseWord.Length)
             {
-                if (!string.IsNullOrEmpty(ToCheck[i]))
+                SplitFromLetterCount++;
+                string SplittedWord = BaseWord.Substring(0, SplitFromLetterCount);
+                string SplittedWord2 = BaseWord.Substring(SplitFromLetterCount);
+                if (ReferenceWords.Contains(SplittedWord) && ReferenceWords.Contains(SplittedWord2))
                 {
-                    if (WordToSplit.ToLower().Contains(ToCheck[i]))
-                    {
-                        Regex rex = new Regex(ToCheck[i], RegexOptions.IgnoreCase);
-                        ToCheck2.Add(rex.Replace(WordToSplit, "", 1));
-                    }
+                    result = SplittedWord + "," + SplittedWord2;
+                    break;
+                }
+                else
+                {
+                    IsWord(BaseWord, ReferenceWords, SplitFromLetterCount);
                 }
             }
-            var matchedvalue = ToCheck.Intersect(ToCheck2);
-            result = string.Join(",", matchedvalue);
-
             return result;
         }
     }
